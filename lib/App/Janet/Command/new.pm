62a3b5a8ec7a406e21e3b2c7886e0900e8c6c180
Michal Wojciechowski <odyniec@cpan.org>
2014-04-29 23:33:35 +0200

    Add support for options for the "new" command

--- lib/App/Janet/Command/new.pm
+++ lib/App/Janet/Command/new.pm
@@ -3,11 +3,21 @@ package App::Janet::Command::new;
 use Cwd;
 use File::Path qw(make_path);
 use File::Spec;
+use Getopt::Long qw(GetOptionsFromArray);
 use Moo;
 
+my %options = (
+    force => 0,
+    blank => 0,
+);
+
 sub process {
-    my ($path) = @_;
-    
+    GetOptionsFromArray(\@_,
+        'force' => \$options{force},
+        'blank' => \$options{blank}
+    );
+    my ($path) = shift;
+
     my $new_blog_path = File::Spec->rel2abs($path);
 
     make_path($new_blog_path);
