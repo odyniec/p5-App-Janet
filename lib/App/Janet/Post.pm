efc21df79d203c3528514900330735eedbca7992
Michal Wojciechowski <odyniec@cpan.org>
2014-05-06 22:24:31 +0200

    Recognize filename extension in posts

--- lib/App/Janet/Post.pm
+++ lib/App/Janet/Post.pm
@@ -7,6 +7,8 @@ use Moo;
 
 with 'App::Janet::Convertible';
 
+my $MATCHER = qr/^(.+\/)*(\d+-\d+-\d+)-(.*)(\.[^.]+)$/;
+
 has 'name' => (
     is => 'ro'
 );
@@ -23,12 +25,29 @@ has 'content' => (
     is => 'rw'
 );
 
+has 'output' => (
+    is => 'rw'
+);
+
+has 'ext' => (
+    is => 'rw'
+);
+
 sub BUILD {
     my ($self) = @_;
 
+    $self->process($self->name);
     $self->read_yaml(undef, $self->name);
 
     return $self;
+}
+
+sub process {
+    my ($self) = @_;
+
+    my ($cats, $date, $slug, $ext) = ($self->name =~ $MATCHER);
+
+    $self->ext($ext);
 }
 
 sub render {
