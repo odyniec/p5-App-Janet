980179cf85eb74e7432adfc9f748a8259a6ba4b1
Michal Wojciechowski <odyniec@cpan.org>
2014-05-07 23:50:28 +0200

    Add URL handling module

---
+++ lib/App/Janet/URL.pm
@@ -0,0 +1,58 @@
+package App::Janet::URL;
+
+use URI::Escape;
+
+sub new {
+    my ($class, %options) = @_;
+
+    return bless {
+        _template => $options{template},
+        _placeholders => $options{placeholders} || {},
+        _permalink => $options{permalink}
+    }, $class;
+}
+
+sub to_s {
+    my ($self) = @_;
+
+    return sanitize_url($self->{_permalink} || $self->generate_url);
+}
+
+sub generate_url {
+    my ($self) = @_;
+
+    my $result = $self->{_template};
+
+    while (($name, $value) = each %{$self->{_placeholders}}) {
+        $value = __PACKAGE__->escape_path($value);
+        $result =~ s/:$name/$value/g;
+    }
+
+    return $result;
+}
+
+sub sanitize_url {
+    my ($in_url) = @_;
+
+    (my $url = $in_url) =~ s{//}{/}g;
+    $url = join('/', grep { !/^\.+$/ } split('/', $url));
+    $url .= '/' if $in_url =~ m{/$};
+    $url =~ s/^([^\/])/\/$1/;
+
+    return $url;
+}
+
+sub escape_path {
+    my ($class, $path) = @_;
+
+    # FIXME: borrowed from Ruby code, not sure if fully compatible with Perl
+    return uri_escape($path, '^a-zA-Z\d\-._~');#!$&\'()*+,;=:@\/');
+}
+
+sub unescape_path {
+    my ($class, $path) = @_;
+
+    return uri_unescape($path);
+}
+
+1;
