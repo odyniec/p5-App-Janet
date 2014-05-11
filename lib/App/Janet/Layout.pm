675a18312eb98391ba44c68967bac89c95d1ffe2
Michal Wojciechowski <odyniec@cpan.org>
2014-05-11 14:01:33 +0200

    Add layout class

---
+++ lib/App/Janet/Layout.pm
@@ -0,0 +1,32 @@
+package App::Janet::Layout;
+
+use Moo;
+
+with 'App::Janet::Convertible';
+
+has 'site' => ( is => 'ro' );
+has 'name' => ( is => 'ro' );
+has 'ext' => ( is => 'rw' );
+has 'data' => ( is => 'rw' );
+has 'content' => ( is => 'rw' );
+
+sub BUILD {
+    my ($self, $args) = @_;
+
+    my $base = delete $args->{'base'};
+
+    $self->process($self->name);
+    $self->read_yaml($base, $self->name);
+
+    return $self;
+}
+
+sub process {
+    my ($self) = @_;
+
+    (my $ext) = ($self->name =~ /(\.[^.]+)$/);
+    
+    $self->ext($ext);
+}
+
+1;
