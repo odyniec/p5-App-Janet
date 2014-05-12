69ab7c3006a0af97cb392877a2331527c0008417
Michal Wojciechowski <odyniec@cpan.org>
2014-05-12 23:35:12 +0200

    Add identity converter

---
+++ lib/App/Janet/Converter/Identity.pm
@@ -0,0 +1,23 @@
+package App::Janet::Converter::Identity;
+
+use strict;
+use warnings;
+
+use Moo;
+use Text::Markdown qw(markdown);
+
+sub matches { 1 }
+
+sub output_ext {
+    my ($self, $ext) = @_;
+
+    return $ext;
+}
+
+sub convert {
+    my ($self, $content) = @_;
+
+    return $content;
+}
+
+1;
