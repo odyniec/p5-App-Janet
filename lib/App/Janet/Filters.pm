42780f8d629b797c32b5a9509c187660d3530e9e
Michal Wojciechowski <odyniec@cpan.org>
2014-05-15 22:30:24 +0200

    Do not convert date into Time::Piece if it already is so

--- lib/App/Janet/Filters.pm
+++ lib/App/Janet/Filters.pm
@@ -5,7 +5,12 @@ use Template::Liquid;
 use Time::Piece;
 
 sub date_to_string {
-    return Time::Piece->strptime($_[0], '%Y-%m-%d')->strftime("%d %b %Y");
+    my ($date) = @_;
+
+    $date = Time::Piece->strptime($date, '%Y-%m-%d')
+        if ref($date) ne 'Time::Piece';
+
+    return $date->strftime("%d %b %Y");
 }
 
 Template::Liquid::register_filter(qw(date_to_string));
