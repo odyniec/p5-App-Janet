1b37bd31eb5d3c72be23059948a514b0ec9a9bef
Michal Wojciechowski <odyniec@cpan.org>
2014-05-16 10:39:32 +0200

    Add class for static files

---
+++ lib/App/Janet/StaticFile.pm
@@ -0,0 +1,61 @@
+package App::Janet::StaticFile;
+
+use strict;
+use warnings;
+
+use File::Basename;
+use File::Copy;
+use File::Path qw(make_path);
+use File::Spec::Functions qw(catfile);
+use File::stat;
+use Moo;
+
+has 'site' => ( is => 'rw' );
+
+has 'base' => ( is => 'rw' );
+
+has 'dir' => ( is => 'rw' );
+
+has 'name' => ( is => 'rw' );
+
+my %mtimes = ();
+
+sub path {
+    my ($self) = @_;
+
+    return catfile($self->base, $self->dir, $self->name);
+}
+
+sub destination {
+    my ($self, $dest) = @_;
+
+    return catfile($dest, $self->dir, $self->name);
+}
+
+sub mtime {
+    my ($self) = @_;
+
+    return stat($self->path)->mtime;
+}
+
+sub modified {
+    my ($self) = @_;
+
+    return $mtimes{$self->path} != $self->mtime;
+}
+
+sub write {
+    my ($self, $dest) = @_;
+
+    my $dest_path = $self->destination($dest);
+
+    return 0 if -e $dest_path && !$self->modified;
+    $mtimes{$self->path} = $self->mtime;
+
+    make_path(dirname($dest_path));
+    copy($self->path, $dest_path);
+
+    return 1;
+}
+
+1;
