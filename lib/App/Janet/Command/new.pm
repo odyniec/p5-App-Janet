06244a5ce3aff5bb92f94f43afd0be2476d7ddb1
Michal Wojciechowski <odyniec@cpan.org>
2014-05-20 23:32:37 +0200

    Extend the new command to create sites with sample files

--- lib/App/Janet/Command/new.pm
+++ lib/App/Janet/Command/new.pm
@@ -4,28 +4,49 @@ use strict;
 use warnings;
 
 use Cwd;
+use File::Copy::Recursive qw(dircopy);
 use File::Path qw(make_path);
+use File::Share;
 use File::Spec;
 use Getopt::Long qw(GetOptionsFromArray);
 use Moo;
+use Time::Piece;
+
+use App::Janet::Utils;
 
 my %options = (
     force => 0,
     blank => 0,
 );
 
+my $scaffold_path = File::Spec->catfile('_posts',
+    '0000-00-00-welcome-to-jekyll.markdown.perl');
+
 sub process {
-    GetOptionsFromArray(\@_,
+    my ($class, $options, @args) = @_;
+
+    GetOptionsFromArray(\@args,
         'force' => \$options{force},
         'blank' => \$options{blank}
     );
-    my ($path) = shift;
+    
+    my $path = shift @args;
 
     my $new_blog_path = File::Spec->rel2abs($path);
 
     make_path($new_blog_path);
     
-    create_blank_site($new_blog_path);
+    if ($options{'blank'}) {
+        create_blank_site($new_blog_path);
+    }
+    else {
+        create_sample_files($new_blog_path);
+
+        open my $f, '>', File::Spec->catfile($new_blog_path,
+            initialized_post_name());
+        print $f scaffold_post_content();
+        close $f;
+    }
 }
 
 sub create_blank_site {
@@ -39,6 +60,34 @@ sub create_blank_site {
     close($fh);
 
     chdir($prev_path);
+}
+
+sub scaffold_post_content {
+    open my $f, '<', File::Spec->catfile(site_template(), $scaffold_path);
+    local $/;
+    my $content = App::Janet::Utils::parse_template(<$f>);
+    close $f;
+
+    return $content;
+}
+
+sub initialized_post_name {
+    File::Spec->catfile('_posts',
+        localtime->strftime('%Y-%m-%d') . '-welcome-to-jekyll.markdown');
+}
+
+sub create_sample_files {
+    my ($path) = @_;
+
+    dircopy(site_template(), $path);
+
+    # Remove scaffold post file
+    unlink File::Spec->catfile($path, $scaffold_path);
+}
+
+sub site_template {
+    return File::Spec->catdir(File::Share::dist_dir('App-Janet'),
+        'site_template');
 }
 
 1;
