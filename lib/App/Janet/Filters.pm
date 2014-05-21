5d22c139ac0d1f7859701074d501dcb9d7823c0b
Michal Wojciechowski <odyniec@cpan.org>
2014-05-21 23:51:15 +0200

    Add date_to_long_string filter

--- lib/App/Janet/Filters.pm
+++ lib/App/Janet/Filters.pm
@@ -13,10 +13,20 @@ sub date_to_string {
     return $date->strftime("%d %b %Y");
 }
 
+sub date_to_long_string {
+    my ($date) = @_;
+
+    $date = Time::Piece->strptime($date, '%Y-%m-%d')
+        if ref($date) ne 'Time::Piece';
+
+    return $date->strftime("%d %B %Y");
+}
+
 sub xml_escape {
     # TODO
 }
 
-Template::Liquid::register_filter(qw(date_to_string xml_escape));
+Template::Liquid::register_filter(qw(date_to_string date_to_long_string
+    xml_escape));
 
 1;
