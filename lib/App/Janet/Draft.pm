f0d46225f83e9df4ddab6978b68fa7c1cd494360
Michal Wojciechowski <odyniec@cpan.org>
2014-06-08 12:15:55 +0200

    Override containing_dir in the Draft class

--- lib/App/Janet/Draft.pm
+++ lib/App/Janet/Draft.pm
@@ -3,11 +3,18 @@ package App::Janet::Draft;
 use strict;
 use warnings;
 
+use File::Spec;
 use Moo;
 
 extends 'App::Janet::Post';
 
 my $MATCHER = qr/^(.*)(\.[^.]+)$/;
+
+sub containing_dir {
+    my ($class, $source, $dir) = @_;
+
+    return File::Spec->catfile($source, $dir, '_drafts');
+}
 
 sub process {
     my ($self) = @_;
