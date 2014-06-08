eff4be2e435b5a0695da972fbf4d83176d4b9249
Michal Wojciechowski <odyniec@cpan.org>
2014-06-08 12:18:42 +0200

    Set date when a draft gets initialized

--- lib/App/Janet/Draft.pm
+++ lib/App/Janet/Draft.pm
@@ -5,6 +5,7 @@ use warnings;
 
 use File::Spec;
 use Moo;
+use Time::Piece;
 
 extends 'App::Janet::Post';
 
@@ -21,6 +22,8 @@ sub process {
 
     my ($slug, $ext) = ($self->name =~ $MATCHER);
 
+    $self->date(Time::Piece->strptime(stat(File::Spec->catfile($self->base,
+        $self->name)), '%s'));
     $self->slug($slug);
     $self->ext($ext);
 }
