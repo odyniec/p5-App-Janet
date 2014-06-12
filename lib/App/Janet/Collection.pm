2ce38ca319eabe81db6a7257b7c9f2252e317ac7
Michal Wojciechowski <odyniec@cpan.org>
2014-06-12 20:08:15 +0200

    Add more methods of the Collection class

--- lib/App/Janet/Collection.pm
+++ lib/App/Janet/Collection.pm
@@ -5,6 +5,8 @@ use warnings;
 
 use Moo;
 
+use App::Janet;
+
 has 'site' => ( is => 'ro' );
 has 'label' => ( is => 'ro' );
 has 'metadata' => ( is => 'ro' );
@@ -22,6 +24,45 @@ sub BUILD {
     my ($self) = @_;
 
     $self->metadata($self->extract_metadata);
+}
+
+sub docs {
+    $self->{'docs'} ||= [];
+}
+
+sub read {
+
+}
+
+sub entries {
+
+}
+
+sub filtered_entries {
+    my ($self) = @_;
+
+    return [] unless $self->exists;
+
+    # TODO: Find documents
+}
+
+sub relative_directory {
+    my ($self) = @_;
+
+    return '_' . $self->label;
+}
+
+sub directory {
+    my ($self) = @_;
+
+    return App::Janet::sanitized_path($self->site->source,
+        $self->relative_directory);
+}
+
+sub exists {
+    my ($self) = @_;
+
+    return (-d $self->directory && !(-l $self->directory && $self->site->safe));
 }
 
 sub extract_metadata {
